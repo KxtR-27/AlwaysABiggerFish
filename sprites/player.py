05752c4aeee3ebf560db96acc9f2db3df55988a4
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2025-08-29 14:05:07 -0400

    Correct aftermath of merge conflicts

--- sprites/player.py
+++ sprites/player.py
@@ -1,11 +1,7 @@
 from sprites import presets 
 from gameSprite import GameSprite
-from sprites import presets 
-from gameSprite import GameSprite
 from utils import *
 
-
-class Player(GameSprite):
 class Player(GameSprite):
     def __init__(self, scene) -> None:
         super().__init__(scene)
